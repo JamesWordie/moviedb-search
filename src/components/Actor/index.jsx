2c56dfe03c6adacdfa44f244aa10cd081be8964a
James Wordie <james.wordie@yahoo.com>
2021-08-29 09:50:25 +0100

    fix to the actor page clickable thumbnail

--- src/components/Actor/index.jsx
+++ src/components/Actor/index.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // style
-import { Wrapper, Image } from './Actor.styles';
+import { Wrapper } from './Actor.styles';
 
 // component
 import ThumbNail from '../ThumbNail';
 
 const Actor = ({ name, character, imageUrl, actorId }) => {
+  // checks to see if the page is the actor's page, if so disable clickable below
+  const actorPage = window.location.href.includes('actor');
+
   return (
     <Wrapper>
-      {/* <Image src={imageUrl} alt='actor-thumbnail' /> */}
       <ThumbNail
         image={imageUrl}
-        clickable={true}
+        clickable={actorPage ? false : true}
         id={actorId}
         optionalPath="/actor"
       />
